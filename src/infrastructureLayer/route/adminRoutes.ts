cf56461ab54ea5e4e09f4918a928917e2c1cb68e
MuhammadNazim6 <nazimnazz66@gmail.com>
2024-06-27 19:22:20 +0530

    Manage admin auth middleware

--- src/infrastructureLayer/route/adminRoutes.ts
+++ src/infrastructureLayer/route/adminRoutes.ts
@@ -5,30 +5,46 @@ import { adminAuth } from "../middleware/authMiddleware";
 
 const router = express();
 
-router.post("/logout", (req: Req, res: Res, next: Next) => {
-  adminAdapter.logoutAdmin(req, res, next);
-});
-router.get("/getProvidersRequests", (req: Req, res: Res, next: Next) => {
-  adminAdapter.getProvidersRequests(req, res, next);
-});
-router.get("/getUsers", (req: Req, res: Res, next: Next) => {
-  adminAdapter.getUsers(req, res, next);
-});
-router.get("/getApprovedProviders", (req: Req, res: Res, next: Next) => {
-  adminAdapter.getApprovedProviders(req, res, next);
-});
-router.patch("/blockUnblockUser", (req: Req, res: Res, next: Next) => {
-  adminAdapter.blockUnblockUser(req, res, next);
-});
-router.patch("/blockUnblockProvider", (req: Req, res: Res, next: Next) => {
-  adminAdapter.blockUnblockProvider(req, res, next);
-});
-router.patch("/acceptReq", (req: Req, res: Res, next: Next) => {
-  adminAdapter.acceptRequest(req, res, next);
-});
-router.patch("/rejectReq", (req: Req, res: Res, next: Next) => {
-  adminAdapter.rejectRequest(req, res, next);
-});
+router.post("/logout",
+  adminAuth,
+  (req: Req, res: Res, next: Next) => {
+    adminAdapter.logoutAdmin(req, res, next);
+  });
+router.get("/getProvidersRequests",
+  adminAuth,
+  (req: Req, res: Res, next: Next) => {
+    adminAdapter.getProvidersRequests(req, res, next);
+  });
+router.get("/getUsers",
+  adminAuth,
+  (req: Req, res: Res, next: Next) => {
+    adminAdapter.getUsers(req, res, next);
+  });
+router.get("/getApprovedProviders",
+  adminAuth,
+  (req: Req, res: Res, next: Next) => {
+    adminAdapter.getApprovedProviders(req, res, next);
+  });
+router.patch("/blockUnblockUser",
+  adminAuth,
+  (req: Req, res: Res, next: Next) => {
+    adminAdapter.blockUnblockUser(req, res, next);
+  });
+router.patch("/blockUnblockProvider",
+  adminAuth,
+  (req: Req, res: Res, next: Next) => {
+    adminAdapter.blockUnblockProvider(req, res, next);
+  });
+router.patch("/acceptReq",
+  adminAuth,
+  (req: Req, res: Res, next: Next) => {
+    adminAdapter.acceptRequest(req, res, next);
+  });
+router.patch("/rejectReq",
+  adminAuth,
+  (req: Req, res: Res, next: Next) => {
+    adminAdapter.rejectRequest(req, res, next);
+  });
 
 
 export default router;
