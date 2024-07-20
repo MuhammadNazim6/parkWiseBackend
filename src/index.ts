437637c0fa5def5bc90b9aeabacdc94b7433f22c
MuhammadNazim6 <nazimnazz66@gmail.com>
2024-07-20 20:21:15 +0530

    Disable cron job

--- src/index.ts
+++ src/index.ts
@@ -18,15 +18,15 @@ const startServer = ()=>{
   })
 }
 
-cron.schedule('*/10 * * * *', () => {
-  axios
-      .get(SERVER)
-      .then((response) => {
-          console.log(`Request sent successfully at ${new Date()}`);
-      })
-      .catch((error) => {
-          console.error(`Error sending request: ${error.message}`);
-      });
-});
+// cron.schedule('*/10 * * * *', () => {
+//   axios
+//       .get(SERVER)
+//       .then((response) => {
+//           console.log(`Request sent successfully at ${new Date()}`);
+//       })
+//       .catch((error) => {
+//           console.error(`Error sending request: ${error.message}`);
+//       });
+// });
 
 startServer()
