f228b0be3826b8796c92184a78a20cbcc47c6a0e
MuhammadNazim6 <nazimnazz66@gmail.com>
2024-07-20 12:21:43 +0530

    Add a cron job for server restarts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
 import {app} from '../src/infrastructureLayer/config/app'
 import { httpServer } from '../src/infrastructureLayer/config/app'
 import connectDB from './infrastructureLayer/config/db'
+import cron from "node-cron";
+import axios from 'axios';
+
+const SERVER = 'https://parkwisebackend.onrender.com'
 
 const startServer = ()=>{
   const port = process.env.PORT || 3000
@@ -13,5 +17,16 @@ const startServer = ()=>{
     connectDB()
   })
 }
+
+cron.schedule('*/10 * * * *', () => {
+  axios
+      .get(SERVER)
+      .then((response) => {
+          console.log(`Request sent successfully at ${new Date()}`);
+      })
+      .catch((error) => {
+          console.error(`Error sending request: ${error.message}`);
+      });
+});
 
 startServer()
